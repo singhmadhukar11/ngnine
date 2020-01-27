7aeac6d622b1327f8bd866933e5a4c1d97a6fa55
madhukar.chouhan <madhukar.chouhan@innoeye.com>
2020-01-27 16:51:00 +0530

    innitial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngnine';
+  title = 'ToDo Application';
+
+  todoList: any = [];
+  todoData;
+  updateTodo = false;
+
+  // localStoreSet = localStorage.setItem("data", this.todoList);
+  // localStoreget = localStorage.getItem("data");
+
+  addTodoData = (data) => {
+    this.todoList.push(data);
+    this.todoData = "";
+  };
+
+  editTodo = (list) => {
+    this.todoData = list;
+    this.updateTodo = true;
+    console.log("Edit");
+  }
+
+  completeTodo = (list) => {
+    let index = this.todoList.indexOf(list);
+    // returnTrue = (index) => { return true; }
+  }
+
+
+  updateTodoData = () => {
+    console.log("Updated Sucessfully");
+    this.updateTodo = false;
+    this.todoData = "";
+  }
+  removeList = (list) => {
+    let index = this.todoList.indexOf(list);
+    this.todoList.splice(index, 1);
+    console.log(list + " Removed");
+  }
+
+  ngOninit(){
+    // console.log(this.localStoreget);
+  }
+
 }
